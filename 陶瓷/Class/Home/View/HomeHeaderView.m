e26522f5de027cd25e462c0691d58a663db1bcdb
郭天龙 <kotanon@kotanondeMacBook-Pro-2.local>
2015-06-13 17:31:28 +0800

    增加  xib

---
+++ 陶瓷/Class/Home/View/HomeHeaderView.m
@@ -0,0 +1,28 @@
+//
+//  HomeHeaderView.m
+//  陶瓷
+//
+//  Created by 郭天龙 on 15/6/13.
+//  Copyright (c) 2015年 王戈. All rights reserved.
+//
+
+#import "HomeHeaderView.h"
+
+@interface HomeHeaderView ()<UIScrollViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) IBOutlet UIPageControl *pageControl;
+
+@end
+
+@implementation HomeHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
