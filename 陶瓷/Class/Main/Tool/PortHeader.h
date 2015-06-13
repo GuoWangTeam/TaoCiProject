f929a575b3bb6bd3838e06302ffb59dac28300ef
Santor <512253863@qq.com>
2015-06-13 18:27:54 +0800

    增加net工具类

---
+++ 陶瓷/Class/Main/Tool/PortHeader.h
@@ -0,0 +1,26 @@
+//
+//  PortHeader.h
+//  demo
+//
+//  Created by 王戈 on 15/6/10.
+//  Copyright (c) 2015年 nice. All rights reserved.
+//
+
+/**
+ *  主路径
+ */
+#define MAIN_PATH @"http://101.200.232.88/"
+/**
+ * 登出
+ */
+#define LOGOUT_URL MAIN_PATH@"Account/Logout"
+/**
+ *  注册
+ */
+#define REGISTER_URL MAIN_PATH@"Account/Register"
+/**
+ *  设置密码
+ */
+#define SET_PASSWORD_URL MAIN_PATH@"Account/SetPassword"
+
+#define GET_USERINFO MAIN_PATH@"Account/GetUserInfo"
