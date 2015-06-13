2a920b923186cb1a5bfd5da6609741fcd55c2cd0
郭天龙 <kotanon@kotanondeMacBook-Pro-2.local>
2015-06-13 18:28:50 +0800

    test

--- 陶瓷/Class/Home/Controller/HomeViewController.m
+++ 陶瓷/Class/Home/Controller/HomeViewController.m
@@ -7,16 +7,51 @@
 //
 
 #import "HomeViewController.h"
+#import "HomeTableViewCell.h"
+#import "HomeHeaderView.h"
+#import "HomeHeadTableViewCell.h"
 
-@interface HomeViewController ()
+@interface HomeViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITableView *tableview;
 
 @end
 
 @implementation HomeViewController
 
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"homeCellId"];
+    if (!cell) {
+        cell = [[NSBundle mainBundle] loadNibNamed:@"HomeTableViewCell" owner:nil options:nil].firstObject;
+    }
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+
+
+#pragma mark - lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.title = @"发现更多";
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,13 +60,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
---
+++ 陶瓷/Class/Home/View/HomeHeadTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeHeadTableViewCell.h
+//  陶瓷
+//
+//  Created by 郭天龙 on 15/6/13.
+//  Copyright (c) 2015年 王戈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeHeadTableViewCell : UITableViewCell
+
+@end
